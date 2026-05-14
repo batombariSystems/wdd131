1cc726f3680eea32a12a8dd3baedd01999c05ae6
batombariSystems <bnyienakuna@byupathway.edu>
2026-05-14 05:12:44 +0100

    date

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,7 +2,7 @@
 
 // Get the current year
 const currentYear = new Date().getFullYear();
-document.getElementById("currentyear").textContent = currentYear;
+document.getElementById("current-year").textContent = currentYear;
 
 // Get the last modified date of the document
 document.getElementById("lastmodified").textContent = `Last Modification: ${document.lastModified}`;
