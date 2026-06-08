899b252d3670cf3fde0d604fb866f000326a9692
batombariSystems <bnyienakuna@byupathway.edu>
2026-06-08 09:06:58 +0100

    footer

--- scripts/form.js
+++ scripts/form.js
@@ -1,31 +1,3 @@
-const products = [
-  {
-    id: "fc-1888",
-    name: "flux capacitor",
-    averagerating: 4.5
-  },
-  {
-    id: "fc-2050",
-    name: "power laces",
-    averagerating: 4.7
-  },
-  {
-    id: "fs-1987",
-    name: "time circuits",
-    averagerating: 3.5
-  },
-  {
-    id: "ac-2000",
-    name: "low voltage reactor",
-    averagerating: 3.9
-  },
-  {
-    id: "jj-1969",
-    name: "warp equalizer",
-    averagerating: 5.0
-  }
-];
-
 const productSelect = document.querySelector("#product");
 
 products.forEach(product => {
@@ -35,5 +7,11 @@ products.forEach(product => {
     option.textContent = product.name;
 
     productSelect.appendChild(option);
-
 });
+
+// Footer information
+document.querySelector("#currentyear").textContent =
+    new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent =
+    document.lastModified;
